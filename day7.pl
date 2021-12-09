78b7030e00ec98868a038668d210ba46c1c601e6
Adam Glass <adamg@microsoft.com>
2021-12-08 16:11:39 -0800

    day8

--- day7.pl
+++ day7.pl
@@ -1,13 +1,44 @@
 :- use_module(library(dcg/basics)).
+:- use_module(library(tabling)).
+:- table crab_p2_fuel/2.
+:- table crab_p1_fuel/2.
 
 crabs([X]) --> integer(X).
 crabs([X|Xs]) --> integer(X), comma, crabs(Xs), blanks.
 
 comma --> [C], {char_code(',', C)}.
 
-day7_p1(File, Fuel):-
+crab_p1_fuel(Distance, Distance).
+
+crab_p2_fuel(Distance, Fuel):-
+    findall(X, between(0, Distance, X), FuelSpend), sum_list(FuelSpend, Fuel).
+
+crab_move_acc([], _, _, AccFuel, AccFuel).
+crab_move_acc([C|Cs], Location, Fuelp, AccFuel, Fuel):-
+    ( Location > C ->
+      Distance is Location - C
+    ;
+      Distance is C - Location
+    ),
+    call(Fuelp, Distance, FuelConsumed),
+    NewAcc is AccFuel + FuelConsumed,
+    crab_move_acc(Cs, Location, Fuelp, NewAcc, Fuel).
+
+crab_move(Crabs, Location, Fuelp, LocFuel):-
+    crab_move_acc(Crabs, Location, Fuelp, 0, LocFuel).
+
+
+day7_common(File, Fuelp, Fuel):-
     phrase_from_file(crabs(Crabs), File),
-    length(Crabs, Fuel).
+    max_list(Crabs, MaxLocation),
+    min_list(Crabs, MinLocation),
+    findall(crab_sol(LocFuel, Location), 
+	    (between(MinLocation, MaxLocation, Location),
+	     crab_move(Crabs, Location, Fuelp, LocFuel)), Sols),
+    min_member(crab_sol(Fuel, _), Sols).
+
+day7_p1(File, Fuel):-
+    day7_common(File, crab_p1_fuel, Fuel).
 
 day7_p1(Fuel):-
     day7_p1("data/day7_p1_data", Fuel).
@@ -15,5 +46,17 @@ day7_p1(Fuel):-
 day7_p1_test(Fuel):-
     day7_p1("data/day7_p1_test", Fuel).
 
+day7_p2(File, Fuel):-
+    day7_common(File, crab_p2_fuel, Fuel).
+
+day7_p2(Fuel):-
+    day7_p2("data/day7_p1_data", Fuel).
+
+day7_p2_test(Fuel):-
+    day7_p2("data/day7_p1_test", Fuel).
+
 day7:-
-    day7_p1_test(37).
+    day7_p1_test(37),
+    day7_p1(337833),
+    day7_p2_test(168),
+    day7_p2(96678050).
---
+++ day8.pl
@@ -0,0 +1,114 @@
+:- use_module(library(dcg/basics)).
+:- use_module(library(tabling)).
+
+entries([ent(P, O)]) --> entry(P,O), eos.
+entries([ent(P, O)|Xs]) --> entry(P,O), entries(Xs).
+
+entry(P, O) --> patterns(P), blank, pipe, blank, patterns(O), blanks.
+pipe --> [P], {char_code('|', P)}.
+space --> [S], {char_code(' ', S)}.
+
+patterns([P]) --> pattern(P).
+patterns([P|Ps]) --> pattern(P), space, patterns(Ps).
+
+pattern([P]) --> alpha(P).
+pattern([P|Ps]) --> alpha(P), pattern(Ps).
+
+alpha(A) --> [A], {code_type(A, alnum)}.
+
+count_1478(L, Count):-
+    findall(C, (member(C, L), length(C, ELength), member(ELength, [2, 3, 4, 7])), Bag), length(Bag, Count).
+
+day8_p1(File, DigitCount):-
+    phrase_from_file(entries(E), File),
+    findall(Count, (member(ent(_, O), E), count_1478(O, Count)), Counts), sum_list(Counts, DigitCount).
+
+day8_p1(Count):-
+    day8_p1("data/day8_p1_data", Count).
+
+day8_p1_test(Count):-
+    day8_p1("data/day8_p1_test2", Count).
+
+xdigit(0, [a,b,c,e,f,g]).
+xdigit(1, [c,f]).
+xdigit(2, [a,c,d,e,g]).
+xdigit(3, [a,c,d,f,g]).
+xdigit(4, [b,c,d,f]).
+xdigit(5, [a,b,d,f,g]).
+xdigit(6, [a,b,d,e,f,g]).
+xdigit(7, [a,c,f]).
+xdigit(8, [a,b,c,d,e,f,g]).
+xdigit(9, [a,b,c,d,f,g]).
+
+wiring(Wires):-
+    char_code('a', Start),
+    char_code('g', End),
+    findall(X, between(Start, End, X), OrigWiring),
+    permutation(OrigWiring, Wires).
+
+check(Val, Assign):-
+    permutation(Val, Assign).
+
+check1(Val, Assign):-
+    check(Val, Assign).
+
+check4(Val, Assign):-
+    check(Val, Assign).
+
+check7(Val, Assign):-
+    check(Val, Assign).
+
+check8(Val, Assign):-
+    check(Val, Assign).
+
+check_three(OneTwoThree, [A,B,C]):-
+    permutation(OneTwoThree, [X,Y,Z]),
+    permutation(X, A),
+    permutation(Y, B),
+    permutation(Z, C).
+
+decode_output(Output, Decoder, Value):-
+    findall(Number, (member(D, Output), permutation(D, DP), nth0(Number, Decoder, DP)), [A,B,C,D]),
+    Value is A*1000+B*100+C*10+D.
+
+decode_entry(ent(Pattern, Output), Value):-
+    wiring([A,B,C,D,E,F,G]),
+    partition([X]>>length(X, 2), Pattern, [One],  Rest),
+    check1(One, [C,F]),
+    partition([X]>>length(X, 4), Rest, [Four], Rest2),
+    check4(Four, [B,C,D,F]),
+    partition([X]>>length(X, 3), Rest2, [Seven], Rest3),
+    check7(Seven, [A,C,F]),
+    partition([X]>>length(X, 7), Rest3, [Eight], Rest4),
+    check8(Eight, [A,B,C,D,E,F,G]),
+    partition([X]>>length(X, 5), Rest4, TwoThreeFive, Rest5),
+    Two = [A,C,D,E,G],
+    Three = [A,C,D,F,G],
+    Five = [A,B,D,F,G],
+    check_three(TwoThreeFive, [Two, Three, Five]),
+    partition([X]>>length(X, 6), Rest5, ZeroSixNine, _),
+    Zero = [A,B,C,E,F,G],
+    Six =  [A,B,D,E,F,G],
+    Nine = [A,B,C,D,F,G],
+    check_three(ZeroSixNine, [Zero, Six, Nine]),
+    Decoder = [Zero, One, Two, Three,Four, Five,Six,Seven,Eight,Nine],
+    decode_output(Output, Decoder, Value).
+
+day8_p2(File, Sum):-
+    phrase_from_file(entries(Entries), File),
+    findall(Number, (member(Entry, Entries),
+		     decode_entry(Entry, Number)), NumberList),
+    sum_list(NumberList, Sum).
+
+day8_p2(Sum):-
+    day8_p2("data/day8_p1_data", Sum).
+
+day8_p2_test(Sum):-
+    day8_p2("data/day8_p1_test2", Sum).
+
+
+day8:-
+    day8_p1_test(26),
+    day8_p1(488),
+    day8_p2_test(61229),
+    day8_p2(1040429).
