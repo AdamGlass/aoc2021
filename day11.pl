857ffea74ee9425b05d0db1c78b3af3e7d81674a
Adam Glass <adamg@microsoft.com>
2021-12-18 10:30:42 -0800

    day11 finally

--- day11.pl
+++ day11.pl
@@ -2,6 +2,7 @@
 :- use_module(library(tabling)).
 :- use_module(library(clpfd)).
 :- use_module(library(assoc)).
+:- use_module(matrix).
 
 matrixp(M) --> mapx(M).
 
@@ -20,59 +21,53 @@ mdigit(V) --> [C], {char_type(C, digit), char_code('0', Zero), V is C - Zero}.
 % energy around increases by 1
 % energy level flashes
 
+dwriteln(A).
+doct_write(Octopi).
 
 oct_flash_octopi_(X+Y, Octopi, FlashedOctopi):-
     matrix(Octopi, X, Y, Value),
-    NewValue is Value + 1,
-    ValueCeiling is min(NewValue, 10),
-    matrix_transform(Octopi, X, Y, ValueCeiling, FlashedOctopi).
+    ( Value < 10 ->
+      NewValue is Value + 1,
+      matrix_transform(Octopi, X, Y, NewValue, FlashedOctopi)
+    ;
+      FlashedOctopi = Octopi
+    ).
 
 oct_flash_octopi_done(Octopi, X+Y, DoneOctopi):-
     matrix_transform(Octopi, X, Y, 11, DoneOctopi).
 
-oct_flash_dir(DX, DY):-
-    member(dir(DX, DY), [dir(-1, -1), dir(0, -1), dir(1,-1),
-                         dir(-1, 0), dir(1, 0),
-                         dir(-1, 1), dir(0, 1), dir(1,1)]).
-
 oct_flash_octopi(flash(X,Y), Octopi, FlashDoneOctopi):-
-    findall(NX+NY,
-	    (oct_flash_dir(DX, DY),
-             NX is X + DX,
-             NY is Y + DY,
-             matrix(Octopi, NX, NY, _)),
-	    EnergyAddList),
+    matrix_xy_adjacent_all(Octopi, X, Y, EnergyAddList),
     foldl(oct_flash_octopi_, EnergyAddList, Octopi, FlashedOctopi),
-    writeln("before done"),
+    dwriteln("before done"),
     oct_flash_octopi_done(FlashedOctopi, X+Y, FlashDoneOctopi),
-    writeln("after done").
+    dwriteln("after done").
 
 oct_flash_(Octopi, FlashingOctopi, PostflashOctopi):-
     foldl(oct_flash_octopi, FlashingOctopi, Octopi, PostflashOctopi).
 
 oct_flash_flashing(Octopi, flash(X,Y)):-
-    matrix_limits(Octopi, XMAX, YMAX),
-    between(0, XMAX, X),
-    between(0, YMAX, Y),
+    matrix_xy(Octopi, X, Y),
     matrix(Octopi, X, Y, 10).
 
+
 oct_flash(Octopi, FlashedOctopi):-
-    writeln(['startflash']),
-    oct_write(Octopi),
+    dwriteln(['startflash']),
+    doct_write(Octopi),
     findall(F,
             oct_flash_flashing(Octopi, F),
             Flashers),
     length(Flashers, Count),
-    writeln(Flashers),
+    dwriteln(Flashers),
     (Count > 0 ->
          [NewF|F] = Flashers,
          FlashingOctopi = [NewF],
          oct_flash_(Octopi, FlashingOctopi, StepFlashedOctopi),
-         writeln(['flashed', FlashingOctopi]),
-         oct_write(StepFlashedOctopi),
+         dwriteln(['flashed_', FlashingOctopi]),
+         doct_write(StepFlashedOctopi),
          oct_flash(StepFlashedOctopi, FlashedOctopi)
     ;
-         writeln("DONE"),
+         dwriteln("DONE"),
          FlashedOctopi = Octopi
     ).
 
@@ -100,52 +95,67 @@ oct_value_char(N, V):-
 oct_value_char(N, 'X'):-
     N > 15.
 
-oct_write(OctoMatrix):-
-    matrix_limits(OctoMatrix, XMAX, YMAX),
-    forall(between(0, YMAX, Y),
-	   (forall(between(0, XMAX, X),
-		   (matrix(OctoMatrix, X, Y, Value),
-		    oct_value_char(Value,Display),
-		    write(Display))),
-	    writeln(""))).
-
 oct_energize(Octopi,NewOctopi):-
     matrix_element_add(1, Octopi, NewOctopi).
 
+
+oct_step(Steps, Octopi, NextStepOctopi, FlashedCount):-
+    dwriteln(["step", Steps]),
+    oct_energize(Octopi, EnergizedOctopi),
+    dwriteln(["energized", Steps]),
+    doct_write(EnergizedOctopi),
+    oct_flash(EnergizedOctopi, FlashedOctopi),
+    dwriteln(["flashed", Steps]),
+    doct_write(FlashedOctopi),
+    dwriteln(["rollover", Steps]),
+    oct_rollover(FlashedOctopi, NextStepOctopi, FlashedCount),
+    dwriteln(["afterrollover", Steps]),
+    doct_write(NextStepOctopi).
+
 oct_steps_(_, 0,          Flashes, Flashes).
 oct_steps_(Octopi, Steps, AccFlashes, Flashes):-
-    writeln(["step", Steps]),
-    oct_energize(Octopi, EnergizedOctopi),
-    writeln(["energized", Steps]),
-    oct_write(EnergizedOctopi),
-    oct_flash(EnergizedOctopi, FlashedOctopi),
-    writeln(["flashed", Steps]),
-    oct_write(FlashedOctopi),
-    writeln(["rollover", Steps]),
-    oct_rollover(FlashedOctopi, NextStepOctopi, FlashedCount),
-    writeln(["afterrollover", Steps]),
-    oct_write(NextStepOctopi),
     NewSteps is Steps - 1,
+    oct_step(Steps, Octopi, NextStepOctopi, FlashedCount),
     NewFlashes is AccFlashes + FlashedCount,
     oct_steps_(NextStepOctopi, NewSteps, NewFlashes, Flashes).
+
+oct_steps_sync_(Octopi, AllCount, Step, Steps):-
+    oct_step(Steps, Octopi, NextStepOctopi, FlashedCount),
+    ( FlashedCount = AllCount ->
+      Steps = Step
+    ;
+      NewSteps is Step + 1,
+      oct_steps_sync_(NextStepOctopi, AllCount, NewSteps, Steps)
+    ).
 
 oct_steps(Octopi, Steps, Flashes):-
     oct_steps_(Octopi, Steps, 0, Flashes).
 
+oct_steps_sync(Octopi, Steps):-
+    matrix_limits(Octopi, XMAX, YMAX),
+    AllCount is (XMAX+1)*(YMAX+1),
+    oct_steps_sync_(Octopi, AllCount, 1, Steps).
+
 day11_p1(File, Steps, Flashes):-
     phrase_from_file(matrixp(ListMatrix), File),
     lmatrix_matrix(ListMatrix, Matrix),
-    oct_write(Matrix),
+    doct_write(Matrix),
     oct_steps(Matrix, Steps, Flashes).
+
+day11_p2(File, Steps):-
+    phrase_from_file(matrixp(ListMatrix), File),
+    lmatrix_matrix(ListMatrix, Matrix),
+    doct_write(Matrix),
+    oct_steps_sync(Matrix, Steps).
 
 day11_p1(Score):-
     day11_p1("data/day11_p1_data", 100, Score).
 
 day11_p1_test(Score):-
-    day11_p1("data/day11_p1_test", 2, Score).
+    day11_p1("data/day11_p1_test", 100, Score).
 
 day11_p1_test2(Score):-
-    day11_p1("data/day11_p1_test2", 1, Score).
+    day11_p1("data/day11_p1_test2", 2, Score).
 
 day11_p2(Score):-
     day11_p2("data/day11_p1_data", Score).
@@ -154,8 +164,7 @@ day11_p2_test(Score):-
     day11_p2("data/day11_p1_test", Score).
 
 day11:-
-    day11_p1_test(_),
-    day11_p1(_),
-    day11_p2_test(_),
-    day11_p2(_).
-
+    day11_p1_test(1656),
+    day11_p1(1717),
+    day11_p2_test(195),
+    day11_p2(476).
