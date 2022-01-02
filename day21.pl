165f8ee4cb298231d7237edaec5978ec546f03af
Adam Glass <adamg@microsoft.com>
2022-01-01 18:18:15 -0800

    undo fever dream approach to p2 redo p1 in 1/5 the complexity

--- day21.pl
+++ day21.pl
@@ -2,7 +2,6 @@
 :- use_module(library(clpfd)).
 :- use_module(library(assoc)).
 :- use_module(library(lists)).
-:- use_module(library(pairs)).
 :- use_module(matrix).
 
 data([D]) --> player_start(D).
@@ -10,8 +9,8 @@ data([D|Ds]) --> player_start(D), data(Ds).
 
 player_start(PS) --> `Player `, integer(N), ` starting position: `, integer(P), blank, {PS = player(N, P)}.
 		 
-make_pstate(player(N, P), State):-
-    State = pstate(N, P, 0).
+game_state([player(N1,P1), player(N2,P2)], GameState):-
+    GameState = gstate(pstate(N1,P1, 0), pstate(N2,P2, 0)).
 
 die(Steps, Offset, Value):-
     TotalSteps is Steps + Offset,
@@ -21,119 +20,39 @@ play_move(P, Roll, NewP, Score):-
     NewP is mod(P + Roll - 1, 10) + 1,
     Score is NewP.
 
-play_turn(pstate(N, P, Score), D1+D2+D3, NewState):-
-    Roll is D1 + D2 + D3,
+play_turn(pstate(N, P, Score), Roll, NewState):-
     play_move(P, Roll, NewP, MoveScore),
     NewScore is Score + MoveScore,
     NewState = pstate(N, NewP, NewScore),
     writeln([player, N, Roll, NewP, NewScore]).
 
-roll_detdie(PlayState, Steps, DieList):-
-    length(PlayState, PlayerCount),
-    DieStepsBase is Steps * PlayerCount *3,
-    findall(D1+D2+D3,
-	   (nth0(N, PlayState, _),
-	    DieSteps is DieStepsBase + N *3,
-	    die(DieSteps, 0, D1),
-	    die(DieSteps, 1, D2),
-	    die(DieSteps, 2, D3)),
-	   DieList).
+roll_det3(Die, Value):-
+    V1 is mod(Die, 100) + 1,
+    V2 is mod(Die+1, 100) + 1,
+    V3 is mod(Die+2, 100) + 1,
+    Value is V1+V2+V3.
 
-play_turn(PlayState, DieList, NewState, WinScore, Winners):-
-    maplist(play_turn, PlayState, DieList, NewState),
-    findall(WinState,
-	    (member(WinState, NewState),
-	     WinState = pstate(_,_, Score),
-	     Score >= WinScore),
-	    Winners).
+has_won(pstate(_, _, Score), Threshold):-
+    Score >= Threshold.
 
-score(Steps, Winners, OldState, NewState, FinalScore):-
-    length(OldState, PlayerCount),
-    DieStepsBase is Steps * PlayerCount *3,
-    min_member(Winner, Winners),
-    nth0(N, NewState, Winner),
-    DieCount is DieStepsBase + N *3 + 3,
-    writeln(DieCount),
-    ( N > 0 ->
-       LoserState = NewState
+play_(gstate(P1, P2), Die, Threshold, FinalScore):-
+    roll_det3(Die, Roll),
+    play_turn(P1, Roll, NewP1),
+    NewDie is Die + 3,
+    ( has_won(NewP1, Threshold) ->
+      P2 = pstate(_,_, LeastScore),
+      FinalScore is NewDie * LeastScore
     ;
-       LoserState = OldState
-    ),
-    findall(LoserScore,
-	    (member(pstate(P, _, LoserScore), LoserState),
-	     P \= N),
-	    LoserScoreList),
-    min_list(LoserScoreList, LoserScore),
-    FinalScore is DieCount * LoserScore.
-
-play_p1_(State, Steps, FinalScore):-
-    roll_detdie(State, Steps, DieList),
-    play_turn(State, DieList, NewState, 1000, Winners),
-    (length(Winners, 0) ->
-	 NewSteps is Steps + 1,
-	 play_p1_(NewState, NewSteps, FinalScore)
-    ;
-         score(Steps, Winners, State, NewState, FinalScore)
+      play_(gstate(P2, NewP1), NewDie, Threshold, FinalScore)
     ).
 
-play_p1(Players, Score):-
-    length(Players, Count),
-    length(State, Count),
-    maplist(make_pstate, Players, State),
-    play_p1_(State, 0, Score).
+play(Players, Score):-
+    game_state(Players, GameState),
+    play_(GameState, 0, 1000, Score).
     
-new_universes_play_turn(Player, Rolls, NewPlayer):-
-    Player = pstate(N, P, Score),
-    play_move(P, Rolls, NewP, MoveScore),
-    NewScore is Score + MoveScore,
-    NewPlayer = pstate(N, NewP, NewScore),
-
-play_universes(State, DieList, NewState):-
-    findall(NewPlayer,
-	    (member(Player, State),
-	    member(Rolls, DieList),
-	    new_universes_play_turn(Player, Rolls, NewPlayer))
-	   NewState).
-
-play_p2_(State, DieUniverses, AccStats, Others):-
-    roll_universes(State, DieList, NewState),
-    partition_winners_l(NewState, Winners, Others),
-    account_winners(W
-    (length(Others, 0) ->
-       writeln("DONE").
-    ;
-       
-    ).
-
-count_sums(A-S, B):-
-    length(S, Count),
-    B = A-Count.
-
-die_universes(DieUniverses):-
-    findall(Total-1,
-	    (between(1, 3, D1),
-	     between(1, 3, D2),
-	     between(1, 3, D3),
-	     Total is D1+D2+D3),
-	    DiesSummed),
-    sort(1, @=<, DiesSummed, SortedDies),
-    group_pairs_by_key(SortedDies, SumGroups),
-    maplist(count_sums, SumGroups, DieUniverses).
-
-play_p2(Players, MaxWinUniverses):-
-    length(Players, Count),
-    length(State, Count),
-    maplist(make_pstate, Players, State),
-    die_universes(DieUniverses),
-    play_p2_(State, DieUniverses, MaxWinUniverses),
-
 day21_p1(File, Score):-
     phrase_from_file(data(Players), File),
-    play_p1(Players, Score).
-
-day21_p2(File, Score):-
-    phrase_from_file(data(Players), File),
-    play_p2(Players, Score).
+    play(Players, Score).
 
 day21_p1(Score):-
     day21_p1("data/day21_p1_data", Score).
