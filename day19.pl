1a179dd269ac3e676b126a4803c923757fb565e6
Adam Glass <adamg@microsoft.com>
2022-01-02 16:00:47 -0800

    day19 infrastructure

--- day19.pl
+++ day19.pl
@@ -3,18 +3,13 @@
 :- use_module(library(assoc)).
 :- use_module(library(lists)).
 
-                                       
+
 scanners([S]) --> scanner(S).
 scanners([S|Ss]) --> scanner(S), blanks, scanners(Ss).
 
-scanner(S) --> scanner_header(H), beacons(D), 
-	       { (H = 0 ->
-		      S = scanner(H, D, 0+0+0)
-		 ;
-		      S = scanner(H, D, X+Y+Z)
-		 )
-	       }.
- 
+scanner(S) --> scanner_header(H), beacons(D),
+	       { S = scanner(H, D) }.
+
 scanner_header(S) --> scanner_prefix, integer(S), scanner_postfix, blanks.
 
 beacons([B]) --> beacon(B).
@@ -26,38 +21,107 @@ comma --> [C], {char_code(',', C)}.
 scanner_prefix --> `--- scanner `.
 scanner_postfix --> ` ---`.
 
-pairwise_distances(scanner(_,Beacons, _), PairWiseDistances):-
+foo(3).
+
+make_pair(H,A,[H,A]).
+%
+% scanner_merge(Scanner, Scanners):-
+%     pairwise_distances(Scanner, Scanner0Distances),
+%     writeln(Scanner0Distances),
+%     maplist(pairwise_distances, Scanners, BeaconDistances),
+%     findall(N-Commonalities,
+% 	    (nth0(N, BeaconDistances, Distances),
+% 	     member(Scanner0, Scanner0Distances),
+% 	     member(Scanner0, Distances),
+% 	     Commonalities = 1),
+% 	    Commons),
+%     group_pairs_by_key(Commons, GroupedCommons),
+%     maplist(count_vals, GroupedCommons, CountedGroupCommons),
+%     writeln(CountedGroupCommons).
+%
+% make_beacon(B):-
+%     B = beacon(X,Y,Z),
+%     [X,Y,Z] ins inf..sup.
+%
+% orient(X+Y+Z, OX+OY+OZ):-
+%     NX #= -X,
+%     NY #= -Y,
+%     NZ #= -Z,
+%     member(AX, [X, NX]),
+%     member(AY, [Y, NY]),
+%     member(AZ, [Z, NZ]),
+%     permutation([AX,AY,AZ], [OX,OY,OZ]).
+%
+% identify_beacon_locations(S0, SharedMostScanner, Beacons):-
+%     S0 = scanner(_,S0Beacons, _).
+%     SharedMostScanner = scanner(_,MBeacons, _).
+%     [X,Y,Z] in inf..sup,
+%     findall(X+Y+Z
+% 		(orient(X+Y+Z, OX+OY+OZ),
+% 		 member(
+% 	  findall(	      member(beacon(SX, SY, SZ), S0Beacons),
+%
+% count_vals(X-L, Count):-
+%     sum_list(L, Count).
+
+
+pairs_([], AccPairs, AccPairs).
+pairs_([H|T], AccPairs, Pairs):-
+    maplist(make_pair(H), T, HPair),
+    append(AccPairs, HPair, NewPairs),
+    pairs_(T, NewPairs, Pairs).
+
+pairs(L, Pairs):-
+    pairs_(L, [], Pairs).
+
+pairwise_distances(Beacons, PairWiseDistances):-
     findall(PairWiseDistance,
-	   (member(R, Beacons),
-	    member(N, Beacons),
-	    R \= N,
-	    R = beacon(RX, RY, RZ),
-	    N = beacon(NX, NY, NZ),
-	    PairWiseDistance is abs(RX-NX) + abs(RY-NY) + abs(RZ-NZ)),
-	   Distances),
-    sort(Distances, PairWiseDistances).
+	    (pairs(Beacons, BeaconPairs),
+	     member([beacon(RX, RY, RZ),beacon(NX, NY, NZ)], BeaconPairs),
+	     PairWiseDistance is abs(RX-NX) + abs(RY-NY) + abs(RZ-NZ)),
+	   PairWiseDistances),
+    writeln(PairWiseDistances).
+label_beacons(Scanner, Current, Scanner):-
+    Current = scanner(N,_),
+    writeln([label_beacons, N]).
 
-count_vals(X-L, Count):-
-    sum_list(L, Count).
+shared_distances(AFingerprint, BFingerprint, Count):-
+    append(AFingerprint, BFingerprint, Fingerprints),
+    sort(Fingerprints, UniqueFingerprints),
+    length(Fingerprints, AllFingerprintsCount),
+    length(UniqueFingerprints, UniqueFingerprintsCount),
+    Count is AllFingerprintsCount - UniqueFingerprintsCount.
 
-scanner_merge(Scanner, Scanners):-
-    pairwise_distances(Scanner, Scanner0Distances),
-    writeln(Scanner0Distances),
-    maplist(pairwise_distances, Scanners, BeaconDistances),
-    findall(N-Commonalities,
-	    (nth0(N, BeaconDistances, Distances),
-	     member(Scanner0, Scanner0Distances),
-	     member(Scanner0, Distances),
-	     Commonalities = 1),
-	    Commons),
-    group_pairs_by_key(Commons, GroupedCommons),
-    maplist(count_vals, GroupedCommons, CountedGroupCommons),
-    writeln(CountedGroupCommons).
+fingerprint_beacons(Scanner, Scanner-Fingerprint):-
+    Scanner = scanner(_, Beacons),
+    pairwise_distances(Beacons, Fingerprint).
+
+identify_shared_most_scanner(GoalFingerprint, ScannerFingerprints, MostSharedScanner-MostSharedFingerprint):-
+    findall(Count-Scanner-Fingerprint,
+	    (member(Scanner-Fingerprint, ScannerFingerprints),
+	     shared_distances(GoalFingerprint, Fingerprint, Count),
+	     Count >= 12),
+	    CommonDistanceCount),
+    max_member(_-MostSharedScanner-MostSharedFingerprint, CommonDistanceCount).
+
+identify_common_beacons(Scanner-_, [], Scanner).
+identify_common_beacons(Scanner-Fingerprint, ScannerFingerprints, UnifiedScanner):-
+    identify_shared_most_scanner(Fingerprint, ScannerFingerprints, MostSharedScanner-MostSharedFingerprint),
+    label_beacons(Scanner, MostSharedScanner, NewScanner),
+    fingerprint_beacons(NewScanner, NewScanner-NewFingerprint),
+    delete(ScannerFingerprints, MostSharedScanner-MostSharedFingerprint, NewScannerFingerprints),
+    identify_common_beacons(NewScanner-NewFingerprint, NewScannerFingerprints, UnifiedScanner).
+
+identify_beacons(Scanners, Beacons):-
+    maplist(fingerprint_beacons, Scanners, ScannerFingerprints),
+    [S0 | Ss] = ScannerFingerprints,
+    identify_common_beacons(S0, Ss, NewS0),
+    scanner(_,Beacons) = NewS0.
 
 day19_p1(File, Score):-
     phrase_from_file(scanners(Scanners), File),
-    [S|Ss] = Scanners,
-    scanner_merge(S, Ss).
+    identify_beacons(Scanners, Beacons),
+    length(Beacons, Score).
 
 day19_parse:-
     phrase_from_file(scanners(B), "data/day19_parse"),
